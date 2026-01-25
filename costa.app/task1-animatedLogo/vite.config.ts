1e1ec2aa9c97af060a791d9005a33ee4726fb11b
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2026-01-25 13:00:14 +0300

    updated the config to make the dist work

--- costa.app/task1-animatedLogo/vite.config.ts
+++ costa.app/task1-animatedLogo/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
+  base: './',
+
   server: {
-    // 'host: true' is critical so your Note 9 can find the server via your IP
     host: true,
     https: true,
   },
